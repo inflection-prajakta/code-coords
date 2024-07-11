c3fcc8f2bf2a771b18736e510179d60fa7f78adb
prajakta.mandlik <prajakta.mandlik@inflectionzone.com>
2024-07-11 16:16:13 +0530

    added tailwind with astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
-import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: node(),
-  integrations: [svelte()],
+  integrations: [tailwind(), svelte()]
 });
